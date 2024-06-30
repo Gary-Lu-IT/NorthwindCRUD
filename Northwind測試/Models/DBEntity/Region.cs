0d97677b0882db72fd9c13a9a89da558d15adbb4
Gary-Lu-IT <reimukaitoku@gmail.com>
2024-06-30 16:33:32 +0800

    Add files via upload

---
+++ Northwind測試/Models/DBEntity/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindControl.Models.DBEntity;
+
+public partial class Region
+{
+    public int RegionID { get; set; }
+
+    public string RegionDescription { get; set; } = null!;
+
+    public virtual ICollection<Territories> Territories { get; set; } = new List<Territories>();
+}
